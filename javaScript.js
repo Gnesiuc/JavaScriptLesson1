561e231ae08abc7722b80379d67c3f7bc0f21f2d
Gnesiuc <gnesiuc11998@gmail.com>
2022-10-19 20:36:09 +0300

    nacalo javascript

---
+++ javaScript.js
@@ -0,0 +1,15 @@
+const text = "How are you?";
+let number = 100
+
+
+console.log(text)
+// alert(text)
+prompt(text)
+
+// text = "Alt text";
+
+
+// console.log(typeof text_1)
+// console.log(text_1)
+
+ 
