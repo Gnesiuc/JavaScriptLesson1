691c613a68a331b9651b7a5796aa1171042e4d61
Gnesiuc <gnesiuc11998@gmail.com>
2022-10-23 16:33:15 +0300

    Peremennie sdelal

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,37 @@
 const text = "How are you?";
-let number = 100
+const text_1 = " Thank You";
+let text_2 = "And you?";
+let number = 100;
+let number_1 = 89;
+let last_name = "Andrei";
+let first_name = " Gnesiuc";
+let question = "How much?"
+let answer = " Expensive"
+let country = "Moldova"
+let city = " Chisinau"
+const number_2 = 69;
+let score = "null"
+const score_1 = "null"
+const score_2 = "null"
+let PowerNow = true;
+let NoPowerNow = false;
 
 
-console.log(text)
-// alert(text)
-prompt(text)
-
-// text = "Alt text";
 
 
-// console.log(typeof text_1)
+
+console.log(text + text_1)
 // console.log(text_1)
-
- 
+// console.log(text_2)
+console.log(number + number_1)
+// console.log(number) 
+// console.log(number_1)
+console.log(last_name + first_name)
+console.log(country + city)
+console.log(question + answer)
+console.log(number_2)
+console.log(score)
+console.log(score_1)
+console.log(score_2)
+console.log(PowerNow)
+console.log(NoPowerNow)
